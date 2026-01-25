4c4cf30f3865da4dd2ee24c914b135814d3a901f
Fauzan Mohammed <fauzanmohammedpp9a@gmail.com>
2026-01-25 10:55:50 -0800

    Import InterviewQuestion and InterviewAnswer classes

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+from .user import User
+from .resume import Resume
+from .interview import Interview
+from .interview_question import InterviewQuestion  # ADD THIS
+from .interview_answer import InterviewAnswer  # If you have this
