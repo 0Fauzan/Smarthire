df33987a1b13656dbd4771208b02fa383b91292d
Fauzan Mohammed <fauzanmohammedpp9a@gmail.com>
2026-02-07 21:15:21 +0530

    Create config.js

---
+++ frontend/js/config.js
@@ -0,0 +1,8 @@
+// frontend/js/config.js
+const CONFIG = {
+    API_BASE_URL: window.location.origin,
+    // If you need different URL for API, uncomment:
+    // API_BASE_URL: window.location.hostname === 'localhost'
+    //     ? 'http://localhost:5000'
+    //     : window.location.origin
+};
